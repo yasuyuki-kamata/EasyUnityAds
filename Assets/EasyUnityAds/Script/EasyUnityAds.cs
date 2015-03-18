4a3ec9ba607a051ceaed01c2a60dc42cc98b6af3
Yasuyuki Kamata <flandre.s199@gmail.com>
2015-03-18 20:17:04 +0900

    initial commit

---
+++ Assets/EasyUnityAds/Script/EasyUnityAds.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class EasyUnityAds : MonoBehaviour
+{
+	[SerializeField]
+	private string gameID_iOS;
+	[SerializeField]
+	private string gameID_Android;
+
+	void Awake ()
+	{
+		if (Advertisement.isSupported) {
+			string gameID;
+#if (UNITY_EDITOR && UNITY_ANDROID) || UNITY_ANDROID
+			gameID = gameID_Android;
+#elif (UNITY_EDITOR && UNITY_IOS) || UNITY_IOS
+			gameID = gameID_iOS;
+#endif
+			if (gameID != "" && gameID != null) {
+				Advertisement.Initialize (gameID);
+			} else {
+				Debug.LogWarning(string.Format("{0} : Does not set User ID!", this.name));
+			}
+		} else {
+			Debug.LogWarning(string.Format("{0} : not supported!", this.name));
+		}
+	}
+
+	public void ShowUnityAds ()
+	{
+		if (Advertisement.isInitialized) {
+			Advertisement.Show();
+		}
+	}
+
+	public bool IsInitialized ()
+	{
+		return Advertisement.isInitialized;
+	}
+}
