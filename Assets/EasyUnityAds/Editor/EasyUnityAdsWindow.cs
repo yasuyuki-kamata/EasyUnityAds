16b52f570be4d64190524baf23e44766052f8e74
Yasuyuki Kamata <flandre.s199@gmail.com>
2015-03-26 00:13:43 +0900

    Added: Helpbox in EasyUnityAdsWindow

--- Assets/EasyUnityAds/Editor/EasyUnityAdsWindow.cs
+++ Assets/EasyUnityAds/Editor/EasyUnityAdsWindow.cs
@@ -4,12 +4,15 @@ using System.Collections;
 
 public class EasyUnityAdsWindow : EditorWindow
 {
+	const string LABEL_HELPBOX_1 = "1) Enter your Game ID";
+	const string LABEL_HELPBOX_2 = "2) Enter your Zone ID (Optional)";
+	const string LABEL_HELPBOX_3 = "3) Create 'UnityAds' GameObject. press button";
 	const string LABEL_GAME_ID = "Game ID";
 	const string LABEL_IOS = "iOS";
 	const string LABEL_ANDROID = "Android";
 	const string LABEL_ZONE_ID = "Zone ID";
 	const string LABEL_CLOSE = "Close";
-	const string LABEL_CREATE_UNITYADS_OBJECT = "Create UnityAds Object";
+	const string LABEL_CREATE_UNITYADS_OBJECT = "Create GameObject";
 	const string GAMEOBJECT_NAME = "UnityAds";
 	private UnityAdsSettings unityAdsSettings;
 	private Texture logo;
@@ -63,6 +66,7 @@ public class EasyUnityAdsWindow : EditorWindow
 
 		GUILayout.Space (20f);
 
+		EditorGUILayout.HelpBox (LABEL_HELPBOX_1, MessageType.None);
 		GUILayout.Label (LABEL_GAME_ID);
 		EditorGUI.indentLevel++;
 		unityAdsSettings.gameID_iOS = EditorGUILayout.TextField (LABEL_IOS, unityAdsSettings.gameID_iOS);
@@ -74,9 +78,11 @@ public class EasyUnityAdsWindow : EditorWindow
 
 		GUILayout.Space (20f);
 
+		EditorGUILayout.HelpBox (LABEL_HELPBOX_2, MessageType.None);
 		GUILayout.Label (LABEL_ZONE_ID);
 		EditorGUI.indentLevel++;
-		unityAdsSettings.zoneID = EditorGUILayout.TextField (LABEL_ZONE_ID, unityAdsSettings.zoneID);
+		string _zoneID = EditorGUILayout.TextField (LABEL_ZONE_ID, unityAdsSettings.zoneID);
+		unityAdsSettings.zoneID = string.IsNullOrEmpty (_zoneID) ? null : _zoneID;
 		EditorGUI.indentLevel--;
 		if (GUILayout.Button ("What is Zone ID?")) {
 			ZoneIDWindow.Init ();
@@ -87,6 +93,7 @@ public class EasyUnityAdsWindow : EditorWindow
 
 		GUILayout.Space (20f);
 
+		EditorGUILayout.HelpBox (LABEL_HELPBOX_3, MessageType.None);
 		if (GUILayout.Button (LABEL_CREATE_UNITYADS_OBJECT)) {
 			CreateUnityAdsObject ();
 		}
